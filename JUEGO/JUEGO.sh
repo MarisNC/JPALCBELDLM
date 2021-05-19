6fbf8473a6ee4c3293c382c4df577f97004dd98d
Marisc0 <69860976+MarisNC@users.noreply.github.com>
2021-05-19 19:33:57 -0400

    Add files via upload

---
+++ JUEGO/JUEGO.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+cd Archivos_de_juego/ARCHIVOS
+chmod +x ./pregunta0.sh
+./pregunta0.sh
+
+chmod +x ./pregunta1.sh
+./pregunta1.sh
+
+cd ~/JUEGO/Archivos_de_juego/CD
+cat texto3.txt
+chmod +x ./pregunta3.sh
+./pregunta3.sh
+
+cd ~/JUEGO/Archivos_de_juego/LS
+chmod +x ./pregunta4.sh
+./pregunta4.sh
+
+chmod +x ./pregunta5.sh	
+./pregunta5.sh
+
+cd ~/JUEGO/Archivos_de_juego/ARCHIVOS
+cat texto6.txt
+chmod +x ./pregunta6.sh	
+./pregunta6.sh
+
+cd ~/JUEGO/Archivos_de_juego/CHMOD
+cat texto7.txt
+chmod +x ./pregunta7.sh	
+./pregunta7.sh 
+
+cd ~/JUEGO/Archivos_de_juego/CD
+chmod +x ./pregunta8.sh	
+./pregunta8.sh
+
+cd ~/JUEGO/Archivos_de_juego/CHMOD
+chmod +x ./pregunta9.sh	
+./pregunta9.sh
+
+cd ~/JUEGO/Archivos_de_juego/TEXT
+cat texto10.txt
+chmod +x ./pregunta10.sh	
+./pregunta10.sh
+
