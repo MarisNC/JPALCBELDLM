c72a9918b9f8df0826eaffaf56ac31264a5db5f9
Marisc0 <69860976+MarisNC@users.noreply.github.com>
2021-05-19 22:23:46 -0400

    Update pregunta100.sh

--- JUEGO/Archivos_de_juego/TEXT/pregunta100.sh
+++ JUEGO/Archivos_de_juego/TEXT/pregunta100.sh
@@ -13,7 +13,22 @@ else
         read -n1 resp
         case $resp in
             [Ss]* )
-                break;;
+		proceso=$(ps aux | grep JUEGO -i | awk -F ' ' '{print $3}' | xargs)
+		read -a newpid <<< "$proceso"
+		proceso=$newpid
+		string1="$proceso"
+
+		proceso2=$(ps aux | grep $1 -i | awk -F ' ' '{print $3}' | xargs)
+		read -a newpid <<< "$proceso2"
+		proceso2=$newpid
+		string2="$proceso2"
+
+		if [ $string1 == $string2 ]; then
+		 exit 0
+		else
+			exit 1
+		fi
+		break;;
             * )
                 echo "" && echo "Salida"
 		exit 1
@@ -21,14 +36,3 @@ else
         esac
     done
 fi
-
-proceso=$(ps aux | grep $1 -i | awk -F ' ' '{print $2}' | xargs)
-read -a newpid <<< "$proceso"
-proceso=$newpid
-if [ $proceso -eq $1 ]; then
- exit 0
-else
-	exit 1
-fi
-
-echo "salida" $?
